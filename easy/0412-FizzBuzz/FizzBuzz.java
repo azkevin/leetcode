6c7f43d3a3081ddfe5bb0672bf020ca3d4cabedd
Kevin Arindaeng <karindaeng@gmail.com>
2022-02-18 20:23:54 -0500

    feat: completed 0000-HelloWorld and 0412-FizzBuzz

---
+++ easy/0412-FizzBuzz/FizzBuzz.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class FizzBuzz {
+    public List<String> fizzBuzz(int n) {
+        List<String> answer = new ArrayList<String>();
+        for (int i = 1; i <= n; i++) {
+            if (i % 15 == 0) {
+                answer.add("FizzBuzz");
+            } else if (i % 5 == 0) {
+                answer.add("Buzz");
+            } else if (i % 3 == 0) {
+                answer.add("Fizz");
+            } else {
+                answer.add(Integer.toString(i));
+            }
+        }
+        return answer;
+    }
+
+    // test
+    public static void main(String args[]) {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        System.out.println(fizzBuzz.fizzBuzz(3));
+        System.out.println(fizzBuzz.fizzBuzz(5));
+        System.out.println(fizzBuzz.fizzBuzz(15));
+    }
+}
